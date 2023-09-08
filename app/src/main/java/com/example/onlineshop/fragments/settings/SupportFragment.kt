41b77667c74085ce143b5ced7c0d36412531d45e
Jabko <jabko@Development-MacBook-Air.local>
2023-09-08 11:53:52 +0200

    Create UI layout for 'Support Messages' fragment and  add toolBar in Support fragment

--- app/src/main/java/com/example/onlineshop/fragments/settings/SupportFragment.kt
+++ app/src/main/java/com/example/onlineshop/fragments/settings/SupportFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.onlineshop.R
@@ -48,6 +49,11 @@ class SupportFragment : Fragment() {
             }
 
         }.attach()
+
+        for (i in 0..3){
+            val textView = LayoutInflater.from(requireContext()).inflate(R.layout.tab_title,null) as TextView
+            binding.tabLayoutSupportFragment.getTabAt(i)?.customView = textView
+        }
 
     }
 }
