cfb5ebb5e8542e7b2efd76e02e7c627586848b87
Jabko <jabko@Development-MacBook-Air.local>
2023-09-08 23:01:13 +0200

    Navigation between 'Supports fragment' and animation are installed.

--- app/src/main/java/com/example/onlineshop/fragments/settings/SupportFragment.kt
+++ app/src/main/java/com/example/onlineshop/fragments/settings/SupportFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.onlineshop.R
 import com.example.onlineshop.databinding.FragmentSupportBinding
+import com.example.onlineshop.helper.hideBottomNavigation
 
 class SupportFragment : Fragment() {
     lateinit var binding: FragmentSupportBinding
@@ -22,7 +25,18 @@ class SupportFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.imageCloseSupportFrag.setOnClickListener {
+            findNavController().navigateUp()
+        }
 
+        binding.butWriteUs.setOnClickListener {
+            findNavController().navigate(R.id.action_supportFragment_to_messageSupportFragment)
+        }
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideBottomNavigation()
     }
 }
--- app/src/main/java/com/example/onlineshop/fragments/settings/supportSections/MessageSupportFragment.kt
+++ app/src/main/java/com/example/onlineshop/fragments/settings/supportSections/MessageSupportFragment.kt
@@ -1,7 +1,32 @@
 package com.example.onlineshop.fragments.settings.supportSections
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.onlineshop.R
+import com.example.onlineshop.databinding.FragmentSupportMessageBinding
 
-class MessageSupportFragment : Fragment(R.layout.fragment_support_message) {
+class MessageSupportFragment : Fragment() {
+    lateinit var binding : FragmentSupportMessageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSupportMessageBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imageCloseSupportFrag.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+    }
 }
